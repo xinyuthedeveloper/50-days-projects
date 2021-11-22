19503166bcd0ff8028dc1853653d441b2d0f45d9
Xin Yu <wilsonyu0626@gmail.com>
2021-11-21 22:05:08 -0800

    profile card responsive with api call and created error handler for 404 response

--- github-profiles/script.js
+++ github-profiles/script.js
@@ -1,5 +1,6 @@
 const APIURL = 'https://api.github.com/users/';
 
+const main = document.getElementById("main");
 const form = document.getElementById("form");
 const search = document.getElementById("search");
 
@@ -10,16 +11,54 @@ form.addEventListener("submit", event => {
 
     if (user) {
         getUser(user);
-        
+
     }
 })
 
 async function getUser(username) {
     try {
         const { data } = await axios(`${APIURL + username}`);
-        console.log(data)
+        createUserProfile(data)
     } catch(err) {
-        console.log(err);
+        if(err.response.status == 404) {
+            createErrorCard("No profile found with this username");
+        }
     }
     
+}
+
+function createUserProfile(user) {
+    const profileElement = `
+    <div class="card">
+        <div>
+            <img src="${user.avatar_url}" alt="${user.name}" class="avatar">
+        </div>
+        <div class="user-info">
+            <h2>${user.name}</h2>
+            <p>
+                ${user.bio}
+            </p>
+            <ul>
+                <li>${user.followers} <strong>Followers</strong></li>
+                <li>${user.following} <strong>Following</strong></li>
+                <li>${user.public_repos} <strong>Repos</strong></li>
+            </ul>
+        <div id="repos">
+            <a href="#" class="repos">Repo 1</a>
+            <a href="#" class="repos">Repo 1</a>
+            <a href="#" class="repos">Repo 1</a>
+        </div>
+        </div>
+    </div>
+    ` 
+    main.innerHTML = profileElement;
+}
+
+function createErrorCard(message) {
+    const element = `
+        <div class="card">
+            <h1>${message}</h1>
+        </div>
+    `
+    main.innerHTML = element;
 }
